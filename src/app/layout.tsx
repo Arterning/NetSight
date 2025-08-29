8d9b3e949e34453db183254c0b89f7780fcd8828
ning <ning.huang.ridson@gmail.com>
2025-08-29 15:44:19 +0800

    Fuck google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from 'next';
-import { Inter, Source_Code_Pro } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 import { AppHeader } from '@/components/app-header';
-
-const fontBody = Inter({
-  subsets: ['latin'],
-  variable: '--font-body',
-});
-
-const fontCode = Source_Code_Pro({
-  subsets: ['latin'],
-  variable: '--font-code',
-});
 
 export const metadata: Metadata = {
   title: 'NetSight',
@@ -29,18 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Source+Code+Pro:wght@400;500&display=swap"
-          rel="stylesheet"
-        />
       </head>
       <body
         className={cn(
           'min-h-screen bg-background font-body antialiased',
-          fontBody.variable,
-          fontCode.variable
         )}
       >
         <ThemeProvider>

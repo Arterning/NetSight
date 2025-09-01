c6355acd9b69a9efb70f729f73500a6b07322d7d
ning.huang <ning.huang.ridson@gmail.com>
2025-09-01 17:00:20 +0800

    scan shell

---
+++ test/http/scan.sh
@@ -0,0 +1,6 @@
+curl --location --request POST 'http://192.168.200.183:9002/api/scan' --header 'User-Agent: Apifox/1.0.0 (https://apifox.com)' --header 'Content-Type: application/json' --data-raw '{
+    "url":"https://icode.pku.edu.cn/",
+    "valueKeywords":["研究", "大学"],
+    "crawlDepth":"level3",
+    "customCrawlDepth":"1",
+}'
